5f40344a14a8203bb73b96152079d789572a672a
NIHAD779 <nigad779@gmail.com>
2024-04-03 23:21:15 +0530

    first commit

--- model.js
+++ model.js
@@ -23,8 +23,107 @@ const commentsSchema = new mongoose.Schema({
   },
 });
 
+const moviesSchema = new mongoose.Schema({
+  plot: {
+    type: String,
+  },
+  genres: {
+    type: Array,
+  },
+  runtime: {
+    type: Number,
+  },
+  cast: {
+    type: Array,
+  },
+  poster: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
+  languages: {
+    type: Array,
+  },
+  released: {
+    type: Date,
+  },
+  directors: {
+    type: Array,
+  },
+  awards: {
+    win: {
+      type: Number,
+    },
+    nominations: {
+      type: Number,
+    },
+    text: {
+      type: String,
+    },
+  },
+  lastUpdated: {
+    type: Date,
+  },
+  year: {
+    type: Number,
+  },
+  imdb: {
+    rating: {
+      type: Number,
+    },
+    votes: {
+      type: Number,
+    },
+    id: {
+      type: Number,
+    },
+  },
+  countires: {
+    type: Array,
+  },
+  type: {
+    type: String,
+  },
+  tomatoes: {
+    viewer: {
+      rating: {
+        type: Number,
+      },
+      numReviews: {
+        type: Number,
+      },
+      meter: {
+        type: Number,
+      },
+    },
+    fresh: {
+      type: Number,
+    },
+    critic: {
+      rating: {
+        type: Number,
+      },
+      numReviews: {
+        type: Number,
+      },
+      meter: {
+        type: Number,
+      },
+    },
+    rotten: {
+      type: Number,
+    },
+    lastUpdated:{
+      type: Date,
+    }
+  },
+});
+
 
 const comments = mongoose.model('comments', commentsSchema)
+const movies = mongoose.model('movies', moviesSchema) 
 module.exports = {
-  comments
+  comments,
+  movies
 };
--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {comments} = require('./model')
+const {movies,comments} = require('./model')
 
 router.get("/comments/:movieID", async (req, res) => {
   try {
@@ -16,6 +16,44 @@ router.get("/comments/:movieID", async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
+
+
+router.get("/movies", async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10; 
+    const skip = (page - 1) * limit; 
+
+    // Filtering parameters
+    let filter = {};
+    if (req.query.title) {
+      filter.title = new RegExp(req.query.title, "i");
+    }
+    if (req.query.year) {
+      filter.year = req.query.year;
+    }
+    const Movies = await movies
+      .find(filter, {
+        title: 1,
+        year: 1,
+        cast: 1,
+      })
+      .skip(skip)
+      .limit(limit);
+
+    const totalCount = await movies.countDocuments(filter);
+
+    res.json({
+      totalPages: Math.ceil(totalCount / limit),
+      currentPage: page,
+      movies: Movies,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 
 
 module.exports = router
